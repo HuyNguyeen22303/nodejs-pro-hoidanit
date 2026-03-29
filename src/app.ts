4e30718c9040c289c1a7ebd6d1814113c3cd4053
unknown <Huynguyeen22303@gmail.com>
2026-03-29 15:30:06 +0700

    video 53

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,29 @@
 // const express = require("express");
 import express from "express";
-import 'dotenv/config'
+import 'dotenv/config';
+
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+
+
+//config view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+
 app.get("/", (req, res) => {
-    res.send("Hello world! ádasdsaddsa");
+    res.render('home.ejs', { title: 'Hey', message: 'Hello there!' })
 })
 
 
 app.get("/test", (req, res) => {
     res.send("Hello test!");
+})
+
+app.get("/abc", (req, res) => {
+    res.send("Hello abc!");
 })
 
 

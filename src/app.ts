14f22404c716c5fd6bc972417e598efd3fdfe207
unknown <Huynguyeen22303@gmail.com>
2026-03-29 15:02:23 +0700

    video 30

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 // const express = require("express");
 import express from "express";
+import 'dotenv/config'
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-    res.send("Hello world update!");
+    res.send("Hello world! ádasdsaddsa");
 })
 
 
@@ -15,4 +16,5 @@ app.get("/test", (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`My app is running on port: ${PORT}`);
+    console.log(`Port .env ${process.env.PORT}`);
 })

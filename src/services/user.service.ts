abd9de61ca6314d3a7d60a1f631ffcd96f0fb73e
unknown <Huynguyeen22303@gmail.com>
2026-03-31 00:03:12 +0700

    video 69

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,16 +2,22 @@ import mysql from 'mysql2/promise';
 import getConnection from '../config/database';
 
 
-const handleCreateUser = (fullName: string, email: string, address: string) => {
+const handleCreateUser = async (fullName: string, email: string, address: string) => {
+
+    const connection = await getConnection();
+    try {
+        const sql = 'INSERT INTO `users`(`name`, `email`, `address`) VALUES (?, ?, ?)';
+        const values = [fullName, email, address];
+
+        const [result, fields] = await connection.execute(sql, values);
 
 
-    // insert to database
+    } catch (err) {
+        console.log(err);
+    }
 
 
 
-
-    // return result
-    console.log("insert a user");
 }
 
 

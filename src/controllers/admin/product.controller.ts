738ef73ab191dd7fe0f91eda38f1e4e70f26bf3a
unknown <Huynguyeen22303@gmail.com>
2026-04-09 21:51:16 +0700

    video 112 validate 2

--- src/controllers/admin/product.controller.ts
+++ src/controllers/admin/product.controller.ts
@@ -1,20 +1,60 @@
 import { Request, Response } from "express";
 import { productSchema, TProductSchema } from "../../validation/product.schema";
+import { array } from "zod";
 
 
 const getAdminCreateProductPage = async (req: Request, res: Response) => {
 
+    const errors: [] = [];
+    const oldData = {
+        name: "",
+        price: "",
+        detailDesc: "",
+        shortDesc: "",
+        quantity: "",
+        factory: "",
+        target: "",
 
-    return res.render('admin/product/create.ejs');
-}
-const postAdminCreateProduct = async (req: Request, res: Response) => {
-    const { name } = req.body as TProductSchema;
-    try {
-        const result = productSchema.parse(req.body);
-        console.log("Oke", result);
-    } catch (error) {
-        console.log(error);
     }
+    return res.render('admin/product/create.ejs', {
+        errors, oldData
+    });
+}
+
+
+
+const postAdminCreateProduct = async (req: Request, res: Response) => {
+    const { name,
+        price,
+        detailDesc,
+        shortDesc,
+        quantity,
+        factory,
+        target } = req.body as TProductSchema;
+    const validate = productSchema.safeParse(req.body);
+
+    // error
+    if (!validate.success) {
+        const errorZod = validate.error?.issues;
+        const errors = errorZod?.map(item => `${item.message} (${item.path})`);
+        const oldData = {
+            name,
+            price,
+            detailDesc,
+            shortDesc,
+            quantity,
+            factory,
+            target
+        }
+        return res.render('admin/product/create.ejs', {
+            errors, oldData
+
+        });
+    }
+
+
+    // succes
+
     return res.redirect("/admin/product")
 }
 
--- src/validation/product.schema.ts
+++ src/validation/product.schema.ts
@@ -2,13 +2,22 @@ import * as z from "zod";
 
 export const productSchema = z.object({
 
-    name: z.string().trim().min(1, { message: 'Vui lòng nhập name tối thiểu là 1 ký tự' }),
-    price: z.number().min(1, { message: 'Vui lòng nhập price tối thiểu là 1' }),
-    detailDesc: z.string().trim().min(1, { message: 'Vui lòng nhập detailDesc sản phẩm tối thiểu là 1 ký tự' }),
-    shortDesc: z.string().trim().min(1, { message: 'Vui lòng nhập shortDesc sản phẩm tối thiểu là 1 ký tự' }),
-    quantity: z.number().min(1, { message: 'Vui lòng nhập quantity tối thiểu là 1' }),
-    factory: z.number().min(1, { message: 'Vui lòng nhập factory tối thiểu là 1 ký tự' }),
-    target: z.number().min(1, { message: 'Vui lòng nhập target sản phẩm tối thiểu là 1 ký tự' }),
+    name: z.string().trim().min(1, { message: 'Tên không được để trống tối thiểu là 1 ký tự' }),
+    price: z.string()
+        .transform((val) => (val === "" ? 0 : Number(val)))
+        .refine((num) => num > 0, {
+            message: "Số tiền tối thiểu là 1",
+        }),
+
+    detailDesc: z.string().trim().min(1, { message: 'detailDesc không được để trống tối thiểu là 1 ký tự' }),
+    shortDesc: z.string().trim().min(1, { message: 'shortDesc không được để trống tối thiểu là 1 ký tự' }),
+    quantity: z.string()
+        .transform((val) => (val === "" ? 0 : Number(val)))
+        .refine((num) => num > 0, {
+            message: "Số lượng tối thiểu là 1",
+        }),
+    factory: z.string().trim().min(1, { message: 'factory không được để trống tối thiểu là 1 ký tự' }),
+    target: z.string().trim().min(1, { message: 'target không được để trống tối thiểu là 1 ký tự' }),
 });
 
 export type TProductSchema = z.input<typeof productSchema>;
